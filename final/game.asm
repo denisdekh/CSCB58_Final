b303d5f05cf8b13e0f44fe7013564a5271d806c5
Denis D <37584435+Umenemo@users.noreply.github.com>
2022-03-27 17:10:37 -0400

    Create game.asm

---
+++ final/game.asm
@@ -0,0 +1,56 @@
+#####################################################################
+#
+# CSCB58 Winter 2022 Assembly Final Project
+# University of Toronto, Scarborough
+#
+# Student: Denis Dekhtyarenko, 1006316675, dekhtyar, d.dekhtyarenko@mail.utoronto.ca
+#
+# Bitmap Display Configuration:
+# - Unit width in pixels: 4 (update this as needed)
+# - Unit height in pixels: 4 (update this as needed)
+# - Display width in pixels: 256 (update this as needed)
+# - Display height in pixels: 256 (update this as needed)
+# - Base Address for Display: 0x10008000 ($gp)
+#
+# Which milestones have been reached in this submission?
+# (See the assignment handout for descriptions of the milestones)
+# - Milestone 1/2/3 (choose the one the applies)
+#
+# Which approved features have been implemented for milestone 3?
+# (See the assignment handout for the list of additional features)
+# 1. (fill in the feature, if any)
+# 2. (fill in the feature, if any)
+# 3. (fill in the feature, if any)
+# ... (add more if necessary)
+#
+# Link to video demonstration for final submission:
+# - (insert YouTube / MyMedia / other URL here). Make sure we can view it!
+#
+# Are you OK with us sharing the video with people outside course staff?
+# - yes / no / yes, and please share this project github link as well!
+#
+# Any additional information that the TA needs to know:
+# - (write here, if any)
+#
+#####################################################################
+
+# Bitmap display starter code
+#
+# Bitmap Display Configuration:
+# - Unit width in pixels: 4
+# - Unit height in pixels: 4
+# - Display width in pixels: 256
+# - Display height in pixels: 256
+# - Base Address for Display: 0x10008000 ($gp)
+#
+.eqv BASE_ADDRESS 0x10008000
+.text
+	li $t0, BASE_ADDRESS # $t0 stores the base address for display
+	li $t1, 0xff0000 # $t1 stores the red colour code
+	li $t2, 0x00ff00 # $t2 stores the green colour code
+	li $t3, 0x0000ff # $t3 stores the blue colour code
+	sw $t1, 0($t0) # paint the first (top-left) unit red.
+	sw $t2, 4($t0) # paint the second unit on the first row green. Why $t0+4?
+	sw $t3, 128($t0) # paint the first unit on the second row blue. Why +256?
+	li $v0, 10 # terminate the program gracefully
+syscall
